f8ef7dfd3e7f8632a070f5bab6fed840b2e1f165
Victor Stone <stone.victor.p@gmail.com>
2016-06-22 18:39:52 -0700

    added timesort<empty> and used typedef in introsort

--- sorting/introsort.hpp
+++ sorting/introsort.hpp
@@ -14,7 +14,8 @@ namespace Introsort {
 
 	template<class RandomIt, class SizeType, class Compare>
 	void SortLimitedDepth(RandomIt first, RandomIt end, SizeType depthLimit, Compare Comp) {
-		while ((end-first) > static_cast<typename std::iterator_traits<RandomIt>::difference_type>(SortThreshold)) {
+		typedef typename std::iterator_traits<RandomIt>::difference_type DiffType;
+		while ((end-first) > static_cast<DiffType>(SortThreshold)) {
 			if (depthLimit == 0) {
 				//Hit our depth limit, do heapsort now
 				Heapsort::Sort(first, end, Comp);
@@ -31,7 +32,8 @@ namespace Introsort {
 
 	template<class RandomIt, class Compare = std::less<typename std::iterator_traits<RandomIt>::value_type>>
 	void Sort(RandomIt first, RandomIt end, Compare Comp = Compare()) {
-		typename std::iterator_traits<RandomIt>::difference_type length = end-first;
+		typedef typename std::iterator_traits<RandomIt>::difference_type DiffType;
+		DiffType length = end-first;
 		int maxDepth = 2 * static_cast<int>(log2(length));
 		SortLimitedDepth(first, end, maxDepth, Comp);
 		//This leaves unsorted chunks of no more than size 'SortThreshold'
