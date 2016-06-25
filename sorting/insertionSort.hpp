f665e0fe35c0eab722b075b9b0f58b1132657711
Victor Stone <stone.victor.p@gmail.com>
2016-06-25 15:25:55 -0700

    implemented insertion sort using std::rotate (closes #2)

--- sorting/insertionSort.hpp
+++ sorting/insertionSort.hpp
@@ -15,8 +15,8 @@ namespace InsertionSort {
 		
 		for (unsigned int i=1; i<length; ++i) {
 			ValueType val = *(first+i);
-			int newPos = i-1;
-			while (newPos > 0 && Comp(val, *(first+newPos))) {
+			int newPos = i;
+			while (newPos > 0 && Comp(val, *(first+newPos-1))) {
 				// *(first+newPos+1) = *(first+newPos);
 				--newPos;
 			}
