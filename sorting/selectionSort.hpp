b068384815652fb769105d1ca5dee0a1ca75b22c
Victor Stone <vstone@wdtablesystems.com>
2016-06-28 13:31:47 -0700

    refined

--- sorting/selectionSort.hpp
+++ sorting/selectionSort.hpp
@@ -1,18 +1,17 @@
-#include <algorithm>
-#include <vector>
+#ifndef SELECTIONSORT_HPP
+#define SELECTIONSORT_HPP 1
 
-#ifndef _SELECTIONSORT_HPP
-#define _SELECTIONSORT_HPP 1
+#include <algorithm>
 
 namespace SelectionSort {
 
-	template<class RandomIt, class Compare = std::less<typename std::iterator_traits<RandomIt>::value_type>>
+	template<class RandomIt, class Compare = std::less<>>
 	void Sort(RandomIt first, RandomIt end, Compare Comp = Compare()) {
 		while (first != end) {
 			std::iter_swap(first, std::min_element(first, end, Comp));
 			++first;
 		}
 	}
-}
+} //namespace SelectionSort
 
-#endif //_INSERTIONSORT_HPP
+#endif //SELECTIONSORT_HPP
