e32f563861436aa9ab676b8b02d5696fb1ede567
Victor Stone <stone.victor.p@gmail.com>
2016-07-10 18:44:14 -0700

    did some inserts and gets from the binary tree

--- trees/test/timeTest.cpp
+++ trees/test/timeTest.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <random>
+#include <vector>
 #include "timeUtility.hpp"
 #include "binaryTree.hpp"
 
@@ -7,11 +9,24 @@ using namespace std;
 typedef chrono::nanoseconds DurationType;
 
 int main() {
+	random_device rd;
+	vector<unsigned int> seeds;
+	seeds.reserve(mt19937::state_size);
+	for (size_t i=0; i<mt19937::state_size; ++i) {
+		seeds.emplace_back(rd());
+	}
+	seed_seq s(seeds.begin(), seeds.end());
+	mt19937 eng(s);
+
 	DurationType duration;
 	{
 		Timer<DurationType> t(&duration);
 		BinaryTree::BinaryTree<int> tree;
-		tree.Insert(1);
+		for (int i=0; i<1000000; ++i) {
+			uniform_int_distribution<int> dist(0,1000000000);
+			tree.Insert(dist(eng));
+		}
+		cout << "size " << tree.Size() << " & height " << tree.Height() << endl;
 	}
 	printf("%.10lf\n",chrono::duration_cast<chrono::duration<double>>(duration).count());
 	return 0;
