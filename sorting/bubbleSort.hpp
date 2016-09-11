ab63330a61468719a6db9b5d7b4e22591219c185
Victor Stone <stone.victor.p@gmail.com>
2016-09-11 11:48:57 -0700

    refactored to no longer require bidirectional iterators

--- sorting/bubbleSort.hpp
+++ sorting/bubbleSort.hpp
@@ -5,27 +5,27 @@
 
 namespace BubbleSort {
 
-	template<class ForwardIt, class Compare = std::less<>, typename = std::enable_if_t<std::is_base_of<std::forward_iterator_tag, typename std::iterator_traits<ForwardIt>::iterator_category>::value>>
-	//std::distance, std::next, and std::prev require the passed iterator to be an InputIterator
+	template<class ForwardIt, class Compare = std::less<>, typename = std::enable_if_t<std::is_base_of<std::bidirectional_iterator_tag, typename std::iterator_traits<ForwardIt>::iterator_category>::value>>
+	//std::distance and std::next require the passed iterator to be an InputIterator
 	//std::iter_swap requires the passed iterator to be a ForwardIterator
 	void Sort(ForwardIt begin, ForwardIt end, Compare Comp = Compare()) {
-		ForwardIt tempEnd = end;
+		auto unsortedLength = std::distance(begin, end);
 
-		while (std::distance(begin, tempEnd) > 1) {
-			//A potentially unsorted list exist [begin,tempEnd) with length at least 2
-			ForwardIt it = std::next(begin);
-			ForwardIt newTempEnd = std::prev(tempEnd);
-			while (it != tempEnd) {
-				ForwardIt previousIt = std::prev(it);
-				if (Comp(*it, *previousIt)) {
-					//need to swap because 'previousIt' belongs to the right of 'it'
-					std::iter_swap(it, previousIt);
-					newTempEnd = std::next(it);
+		while (unsortedLength > 1) {
+			//A potentially unsorted list exists with length at least 2
+			ForwardIt leftIt = begin;
+			ForwardIt rightIt = std::next(begin);
+			auto newUnsortedLength = 0;
+			for (decltype(unsortedLength) i=1; i<unsortedLength; ++i) {
+				if (Comp(*rightIt, *leftIt)) {
+					//need to swap because 'leftIt' belongs to the right of 'rightIt'
+					std::iter_swap(leftIt, rightIt);
+					newUnsortedLength = i;
 				}
-				++it;
+				++leftIt;
+				++rightIt;
 			}
-			//move the end to the left up until the most recent swap
-			tempEnd = newTempEnd;
+			unsortedLength = newUnsortedLength;
 		}
 	}
 }
