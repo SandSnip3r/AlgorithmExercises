773589755cd7918a533862b729746cd3224e0042
Victor Stone <stone.victor.p@gmail.com>
2016-09-11 12:19:56 -0700

    SFINAE magic

--- sorting/insertionSort.hpp
+++ sorting/insertionSort.hpp
@@ -7,17 +7,20 @@
 
 namespace InsertionSort {
 
-	template<class RandomIt, class Compare = std::less<>>
-	void Sort(RandomIt begin, RandomIt end, Compare Comp = Compare()) {
+	template<class BidirIt, class Compare = std::less<>, typename = std::enable_if_t<std::is_base_of<std::bidirectional_iterator_tag, typename std::iterator_traits<BidirIt>::iterator_category>::value>>
+	//std::distance and std::find_if_not require the passed iterator to be an InputIterator
+	//std::rotate requires the passed iterator to be an ForwardIterator
+	//std::reverse_iterator requires the passed iterator to be an BidirectionalIterator
+	void Sort(BidirIt begin, BidirIt end, Compare Comp = Compare()) {
 		if (std::distance(begin,end) < 2) {
 			return;
 		}
 
-		using RevIt = std::reverse_iterator<RandomIt>;
+		using RevIt = std::reverse_iterator<BidirIt>;
 
-		for (RandomIt firstUnsorted=std::next(begin); firstUnsorted!=end; ++firstUnsorted) {
+		for (BidirIt firstUnsorted=std::next(begin); firstUnsorted!=end; ++firstUnsorted) {
 			//Find the first element where 'firstUnsorted' is not less than it
-			const RandomIt insertionPoint = std::find_if_not(RevIt(firstUnsorted), RevIt(begin), [=](const auto &element) {
+			const BidirIt insertionPoint = std::find_if_not(RevIt(firstUnsorted), RevIt(begin), [=](const auto &element) {
 				return Comp(*firstUnsorted,element);
 			}).base();
 			//Rotate to move firstUnsorted into place
